6ac9f028897e29b37f802f62758084dae4d09d66
maguro869 <maguro869negitoro@gmail.com>
2020-09-15 20:53:40 +0900

    config.pyについて

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TwitchVotingWithOBS
 Twitchコメントによる投票機能 for DJxDJ
+
+# Usage
+config.pyに各種パラメータを書く必要があります。
+詳しくはconfig.pyをご覧下さい。
--- config.py
+++ config.py
@@ -1,5 +1,15 @@
-class Config:
-    HOST = 'localhost'
-    PORT = 4444
-    PASSWORD = 'password'
-    MASTER_ID = 'maguro869'
+# obs-websocket-pyのパラメータ
+HOST = 'localhost'      # ローカル環境ならlocalhostでOK
+PORT = 4444             # OBSWebsocketに設定したサーバーポート
+PASSWORD = 'password'   # OBSWebsocketに設定したパスワード
+
+# TwitchIOのパラメータ
+# 各パラメータの取得方法は https://qiita.com/maguro869/items/57b866779b665058cfe8 を参考にしてください
+TMI_TOKEN = 'oauth:xxxxxxxxxxxxxxxxxxxx'
+CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+BOT_NICK = 'twitch_bot_id'
+BOT_PREFIX = '!'
+CHANNEL = ['twitch_id']
+
+# TwitchVotingWithOBSのパラメータ
+MASTER_ID = 'twitch_id' # botの管理者権限を持つTwitchのアカウントのID
